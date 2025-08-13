e327f909cbf044924386b8f0296b47d51c592aa5
Karthik Raj P R <karthikrajpr@Karthiks-MacBook-Air.local>
2025-08-13 18:00:23 +0530

    Capstone final project - java

---
+++ SDET/CapstoneProject_2/src/main/java/base/BaseClass.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+    public static WebDriver driver;
+
+    public static void initializeDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
